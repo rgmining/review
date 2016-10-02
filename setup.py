7ba1c60bd41790354d8334e71d0d07d82bce57b8
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-10-02 03:09:51 -0400

    Add setup.py and requirements.txt.

---
+++ setup.py
@@ -0,0 +1,49 @@
+#
+# setup.py
+#
+# Copyright (c) 2016 Junpei Kawamoto
+#
+# This file is part of rgmining-review.
+#
+# rgmining-review is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# rgmining-review is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Package information of review package for review graph mining project.
+"""
+from setuptools import setup, find_packages
+
+
+def _load_requires_from_file(filepath):
+    """Read a package list from a given file path.
+
+    Args:
+      filepath: file path of the package list.
+
+    Returns:
+      a list of package names.
+    """
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
+
+setup(
+    name='rgmining-review',
+    version='0.9.0',
+    author="Junpei Kawamoto",
+    author_email="kawamoto.junpei@gmail.com",
+    description="Review data structure for Review graph mining project",
+    url="https://github.com/rgmining/common",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=_load_requires_from_file("requirements.txt"),
+    test_suite='tests.suite'
+)
